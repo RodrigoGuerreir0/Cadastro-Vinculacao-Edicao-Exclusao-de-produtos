a215404419bb8e3bdab867f6043274f2a5f5c3c5
Rodrigo Guerreiro <rodrigoguerreiro217@gmail.com>
2024-02-01 08:56:37 -0300

    Add files via upload

---
+++ php_iniciante_crud_level_1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO - Ler Registros - Tutorial de PHP CRUD</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+<body>
+<h1>ola mundo</h1>
+<style>
+    m-r-1em{margin-right:1em;}
+    m-r-1em{margin-top:none;}
+    m-l-1em{margin-left:1em;}
+    m-b-1em{margin-bottom:1em;}
+
+</style>
+
+   
+<!-- jQuery (Necessário para os plugins JavaScript do Bootstrap.) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <!-- JavaScript do Bootstrap mais recente, compilado e minimizado. -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
